359af27589eaf215e3b71470f82587ab771552fe
Ali Bakhsh <160254256+alibakhsh7520@users.noreply.github.com>
2024-05-17 12:26:10 +0500

    3rd commit

--- switch.ts
+++ switch.ts
@@ -2,30 +2,38 @@
 // switch (dayoff){
 //     case "sunday" :
 //         console.log("go outing");
-//         break;    
+//         break;
 //     case "saturday":
 //         console.log("read a book");
 //         break;
 //         default:
-//             console.log("work on a hobby."); 
+//             console.log("work on a hobby.");
 // }
 
 
-let day ="tuesday"
-switch (day){
-    case "sunday":
-        console.log("going to reletives");
-        break;
-    case "monday":
-        console.log("going to office");
-         break; 
-    case "tuesday":
-         console.log("going o governer house ");
-         break;
-    case "wednesday":
-         console.log("going to college");
-         break;
-         default:
-         console.log("sleeeeeeeeeeping");
-    
+let day = "sunday";
+switch (day) {
+  case "sunday":
+    console.log("goving to governer house for attending class");
+    break;
+  case "monday":
+    console.log("going to office");
+    break;
+  case "tuesday":
+    console.log("going to bazar");
+    break;
+  case "wedneday":
+    console.log("going to reletives");
+    break;
+  case "thursday":
+    console.log("going to villege");
+    break;
+  case "friday":
+    console.log("going to Jumma Namaz");
+    break;
+  case "saturday":
+    console.log("rest at Home");
+    break;
+  default:
+    console.log("sooooory");
 }
